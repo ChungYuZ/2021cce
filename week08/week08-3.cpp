f3b1a92c1e104e55aadda21095b12d83f3840026
張宗煜 <91170713+ChungYuZ@users.noreply.github.com>
2021-11-03 11:55:53 +0800

    Add files via upload

---
+++ week08/week08-3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int m;
+	scanf("%d",&m);
+	for(int now=2;now<=m;now++){
+		int bad=0;
+		for(int i=2;i<now;i++){
+			if(now%i==0) bad=1;
+		}
+		if(bad==00) printf("%d ",now);
+	}
+
+}
