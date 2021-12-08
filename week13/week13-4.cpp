871c22158e8bb638c3b22bf4eb25718bcdd7ade6
張宗煜 <91170713+ChungYuZ@users.noreply.github.com>
2021-12-08 11:51:25 +0800

    Add files via upload

---
+++ week13/week13-4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void printstar(int n)
+{
+    for(int i=0;i<n;i++) printf("*");
+
+}
+
+int main()
+{
+    for(int i=0;i<10;i++){
+        printstar(i);
+        printf("\n");
+    }
+
+}
